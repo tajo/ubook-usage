c47ef5b682c038e8c7f71852f06febed804418ec
Vojtech Miksu <vojtech@uber.com>
2020-05-14 23:40:12 -0700

    ok

--- run-snapshots.sh
+++ run-snapshots.sh
@@ -2,7 +2,6 @@
 
 set -u
 
-
 exit_script () {
   cat .buildkite/prompt-update.yml | buildkite-agent pipeline upload
   exit 1
@@ -13,7 +12,7 @@ exit_script
 echo "Downloading master snapshots from $S3_BUCKET_NAME S3 bucket"
 echo
 ./files.js download artifacts
-if [ $? -eq 1 ]; then exit 1; fi
+if [ $? -eq 1 ]; then exit_script; fi
 
 echo
 echo "Collecting stories and capturing snapshots"
@@ -21,12 +20,12 @@ echo "Collecting stories and capturing snapshots"
 if [ "$BUILDKITE_BRANCH" = "master" ]
 then
   yarn ubook-snapshot --host="http://ubook-server" -u
-  if [ $? -eq 1 ]; then exit 1; fi
+  if [ $? -eq 1 ]; then exit_script; fi
   echo
   echo "Updating master snapshots in $S3_BUCKET_NAME S3 bucket"
   ./files.js upload artifacts
 else
   yarn ubook-snapshot --host="http://ubook-server"
 fi
-if [ $? -eq 1 ]; then exit 1; fi
-
+if [ $? -eq 1 ]; then exit_script; fi
+exit 0
