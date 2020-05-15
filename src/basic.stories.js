092f1a91e8acbd0793f632cbbae2696467826a75
Vojtech Miksu <vojtech@uber.com>
2020-05-15 10:32:24 -0700

    not working

--- src/basic.stories.js
+++ src/basic.stories.js
@@ -10,7 +10,7 @@ export default {
 export const Primary = () => (
   <HeadingLevel>
     <Heading styleLevel={3}>Primary</Heading>
-    <Button onClick={action("button-click")}>Buttonb</Button>
+    <Button onClick={action("button-click")}>Different</Button>
   </HeadingLevel>
 );
 
