11623fce9a19773e8e02ab507357aec59c9eb290
Vojtech Miksu <vojtech@uber.com>
2020-05-27 20:52:57 -0700

    a

--- ubook_snapshot.sh
+++ ubook_snapshot.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+set -m
 
-set -eo pipefail
+echo "Building storybook instance and running server"
 
+# run storybook instance
+npx ubook -o dist
+npx static-server dist --port 51372 &
 
 # Polls for application health page
-echo "Waiting for app at http://web-code-ubook-server:51372"
+echo "Waiting for app at http://localhost:51372"
 
-until $(curl --output /dev/null --silent --head --fail http://web-code-ubook-server:51372); do
+until $(curl --output /dev/null --silent --head --fail http://localhost:51372); do
   echo 'waiting...'
   sleep 1
 done
 
-echo "http://web-code-ubook-server:51372 is available"
+echo "http://localhost:51372 is available"
 
-npx ubook-snapshot -b chromium --host="http://web-code-ubook-server"
+npx ubook-snapshot -b chromium
+
+fg %1
